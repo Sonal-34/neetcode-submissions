1cd381d82e0560a7b27c4f5136cfb1cb21108cfb
Sonal Parashar <154010530+Sonal-34@users.noreply.github.com>
2026-07-24 21:11:45 +0530

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        //using kadane's algorithm
+        int currsum = 0;
+        int maxsum = Integer.MIN_VALUE;
+
+        for(int i=0;i<nums.length;i++)
+        {
+            currsum =  currsum + nums[i];
+            maxsum = Math.max(currsum,maxsum);
+                if(currsum < 0)
+        {
+            currsum = 0;
+        }
+        }
+    
+        return maxsum;
+    }
+}
